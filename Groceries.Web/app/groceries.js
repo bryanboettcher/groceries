3d174b8db48cfc94ce0afef7fb8b06d31fac60b7
Bryan Boettcher <insta@pingring.org>
2017-12-18 14:21:03 -0600

    got a hello world

--- Groceries.Web/app/groceries.js
+++ Groceries.Web/app/groceries.js
@@ -1,4 +1,19 @@
-﻿angular.module('GroceriesApp', [])
-    .controller('HelloWorldController', function ($scope) {
-        $scope.greeting = "Hello World";
-    });
+﻿// define the root application
+var app = angular.module('GroceriesApp', [/* no dependencies */]);
+
+
+app.service('GreetingService', greetingService);
+
+// define the main controller
+app.controller('HelloWorldController', helloWorldControllerFunction);
+
+
+function greetingService() {
+    return {
+        getGreeting: function() { return 'hello world from service'; }
+    };
+}
+
+function helloWorldControllerFunction($scope, GreetingService) {
+    $scope.greeting = GreetingService.getGreeting();
+}
