f18c4bc348a9ec2ee3d04bc19285612cc6071046
Bryan Boettcher <insta@pingring.org>
2017-12-20 13:01:06 -0600

    wiring in some actual code

--- Shopping.Web/app/shopping.js
+++ Shopping.Web/app/shopping.js
@@ -1,6 +1,31 @@
-﻿
-function shoppingController($scope, GreetingService) {
-    $scope.greeting = GreetingService.getGreeting();
-    GreetingService.doVoidMethodThingy();
-    //$scope.greeting = 'hi';
+﻿function shoppingController($scope) {
+
+    $scope.items = [];
+
+    $scope.newname = '';
+    $scope.newquantity = 0;
+
+    $scope.addItem = function(name, quantity) {
+        $scope.items.push({
+            name: name,
+            quantity: quantity
+        });
+
+        $scope.totalItems = $scope.items.length;
+        $scope.newname = '';
+    }
+
+    $scope.removeItem = function(name) {
+
+        let index;
+        for (index = 0; index < $scope.items.length; index++) {
+            if ($scope.items[index].name === name) {
+                $scope.items.splice(index, 1);
+                return;
+            }
+        }
+
+        $scope.totalItems = $scope.items.length;
+    };
+
 }
--- Shopping.Web/app/spec/shopping.js
+++ Shopping.Web/app/spec/shopping.js
@@ -1,18 +1,65 @@
 ﻿describe('Hello World Controller', function() {
 
-    var subject, scope, service;
+    var subject, scope;
 
     beforeEach(function() {
-
         scope = {};
-        subject = new helloWorldControllerFunction(scope, service);
+        subject = new shoppingController(scope);
     });
 
-    describe('When loading the controller', function() {
-
-        beforeEach(function() {
-        });
+    describe(' :: When loading the controller', function() {
         
+        it('Should create an empty items array', function() {
+            expect(scope.items).not.toBeNull();
+        });
+
+        it('Should initialize the newname property', function() {
+            expect(scope.newname).toBeFalsy();
+        });
+
+        describe(' :: When adding items', function() {
+            beforeEach(function () {
+                scope.newname = 'my item';
+                scope.addItem('my item', 3);
+            });
+
+            it('Should add a single item', function () {
+                expect(scope.items.length).toBe(1);
+            });
+
+            it('Should add the right name', function () {
+                expect(scope.items[0].name).toBe('my item');
+            });
+
+            it('Should add the right quantity', function () {
+                expect(scope.items[0].quantity).toBe(3);
+            });
+
+            it('Should blank the newname property', function () {
+                expect(scope.newname).toBeFalsy();
+            });
+
+            describe(' :: When removing known items', function() {
+                beforeEach(function () {
+                    scope.removeItem('my item');
+                });
+
+                it('Should remove the item', function () {
+                    expect(scope.items.length).toBe(0);
+                });
+            });
+
+            describe(' :: When removing unknown items', function () {
+                beforeEach(function() {
+                    scope.removeItem('some item');
+                });
+
+                it('Should not remove the item', function() {
+                    expect(scope.items.length).toBe(1);
+                });
+            });
+        });
+
     });
 
 });
