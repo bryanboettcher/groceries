0b20f51858bbeddf237ede1f4f2d2cd21c940a08
Bryan Boettcher <insta@pingring.org>
2017-12-21 13:22:12 -0600

    adding more comprehensive unit tests and additional functionality

--- Shopping.Web/app/shopping.js
+++ Shopping.Web/app/shopping.js
@@ -1,31 +1,77 @@
-﻿function shoppingController($scope) {
+﻿'use strict';
+
+function shoppingController($scope) {
 
     $scope.items = [];
+    $scope.cart = [];
+
+    $scope.webtext = {
+        add: 'Add',
+        remove: 'Remove',
+        purchase: 'Move to cart'
+    };
 
     $scope.newname = '';
     $scope.newquantity = 0;
 
-    $scope.addItem = function(name, quantity) {
-        $scope.items.push({
-            name: name,
-            quantity: quantity
-        });
+    $scope.addItemFromPage = function() {
 
-        $scope.totalItems = $scope.items.length;
+        $scope.addItem($scope.newname, $scope.newquantity);
         $scope.newname = '';
-    }
+        $scope.newquantity = 0;
 
-    $scope.removeItem = function(name) {
+    };
 
-        let index;
-        for (index = 0; index < $scope.items.length; index++) {
-            if ($scope.items[index].name === name) {
-                $scope.items.splice(index, 1);
+    $scope.addItem = function(name, quantity) {
+
+        name = name.trim();
+
+        if (!name) return;
+        if (!quantity) return;
+
+        let list = $scope.items;
+
+        for (let i = 0; i < list.length; i++) {
+            if (list[i].name.toLowerCase() === name.toLowerCase()) {
+                list[i].quantity = quantity;
                 return;
             }
         }
 
-        $scope.totalItems = $scope.items.length;
+        list.push({
+            name: name,
+            quantity: quantity
+        });
     };
+
+    $scope.removeItem = function (name) {
+
+        let index;
+        for (index = 0; index < $scope.items.length; index++) {
+            if ($scope.items[index].name.toLowerCase() === name.toLowerCase()) {
+                $scope.items.splice(index, 1);
+                return;
+            }
+        }
+    };
+
+    $scope.purchaseItem = function(name) {
+
+        let item = undefined;
+        let list = $scope.items;
+
+        for (let i = 0; i < list.length; i++) {
+            if (list[i].name === name) {
+                item = list[i];
+                break;
+            }
+        }
+
+        if (!item) return;
+
+        $scope.cart.push(item);
+
+        $scope.removeItem(name);
+    }
 
 }
---
+++ Shopping.Web/app/spec/shopping-spec.js
@@ -0,0 +1,167 @@
+﻿describe('Shopping Controller', function() {
+
+    var subject, scope;
+
+    beforeEach(function() {
+        scope = {};
+        subject = new shoppingController(scope);
+    });
+
+    describe(' :: When loading the controller', function() {
+        
+        it('Should create an empty items array', function() {
+            expect(scope.items).not.toBeNull();
+        });
+
+        it('Should initialize the newname property', function() {
+            expect(scope.newname).toBeFalsy();
+        });
+
+        it('Should initialize the newquantity property', function () {
+            expect(scope.newquantity).toBeFalsy();
+        });
+
+        describe(' :: When adding valid items', function() {
+            beforeEach(function () {
+                scope.addItem('my item', 3);
+            });
+
+            it('Should add a single item', function () {
+                expect(scope.items.length).toBe(1);
+            });
+
+            it('Should add the right name', function () {
+                expect(scope.items[0].name).toBe('my item');
+            });
+
+            it('Should add the right quantity', function () {
+                expect(scope.items[0].quantity).toBe(3);
+            });
+
+            describe(' :: When adding a duplicate item', function() {
+                beforeEach(function() {
+                    scope.addItem('my item', 5);
+                });
+
+                it('Should not add a new item', function() {
+                    expect(scope.items.length).toBe(1);
+                });
+
+                it('Should update the existing item', function() {
+                    expect(scope.items[0].quantity).toBe(5);
+                });
+            });
+
+            describe(' :: When adding a duplicate item in a different case', function () {
+                beforeEach(function () {
+                    scope.addItem('MY ITEM', 4);
+                });
+
+                it('Should not add a new item', function () {
+                    expect(scope.items.length).toBe(1);
+                });
+
+                it('Should not change the existing item', function () {
+                    expect(scope.items[0].name).toBe('my item');
+                });
+
+                it('Should update the existing item', function () {
+                    expect(scope.items[0].quantity).toBe(4);
+                });
+            });
+
+            describe(' :: When removing known items', function() {
+                beforeEach(function () {
+                    scope.removeItem('my item');
+                });
+
+                it('Should remove the item', function () {
+                    expect(scope.items.length).toBe(0);
+                });
+            });
+
+            describe(' :: When removing known items with a different casing', function () {
+                beforeEach(function () {
+                    scope.removeItem('MY ITEM');
+                });
+
+                it('Should remove the item', function () {
+                    expect(scope.items.length).toBe(0);
+                });
+            });
+
+            describe(' :: When removing unknown items', function () {
+                beforeEach(function() {
+                    scope.removeItem('some item');
+                });
+
+                it('Should not remove the item', function() {
+                    expect(scope.items.length).toBe(1);
+                });
+            });
+        });
+
+        describe(' :: When adding item with default name', function() {
+            beforeEach(function() {
+                scope.addItem('', 1);
+            });
+
+            it('Should not add the item', function() {
+                expect(scope.items.length).toBe(0);
+            });
+        });
+
+        describe(' :: When adding item with quantity 0', function () {
+            beforeEach(function () {
+                scope.addItem('some value', 0);
+            });
+
+            it('Should not add the item', function () {
+                expect(scope.items.length).toBe(0);
+            });
+        });
+
+        describe(' :: When adding item with purely whitespace name', function () {
+            beforeEach(function () {
+                scope.addItem(' ', 1);
+            });
+
+            it('Should not add the item', function () {
+                expect(scope.items.length).toBe(0);
+            });
+        });
+
+        describe(' :: When adding item with whitespace in name', function () {
+            beforeEach(function () {
+                scope.addItem(' spacey name ', 1);
+            });
+
+            it('Should add the item', function () {
+                expect(scope.items.length).toBe(1);
+            });
+
+            it('Should fix the whitespace', function () {
+                expect(scope.items[0].name).toBe('spacey name');
+            });
+
+            describe(' :: When adding second item with whitespace in the name', function() {
+                beforeEach(function() {
+                    scope.addItem('spacey name ', 2);
+                });
+
+                it('Should not add the item', function () {
+                    expect(scope.items.length).toBe(1);
+                });
+
+                it('Should not rename the item', function () {
+                    expect(scope.items[0].name).toBe('spacey name');
+                });
+
+                it('Should update the quantity', function () {
+                    expect(scope.items[0].quantity).toBe(2);
+                });
+            });
+        });
+    });
+
+});
